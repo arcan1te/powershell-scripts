8325fca0deb37f05b82f425d1fa4cf1bd7881f5a
arcan1te <68873126+arcan1te@users.noreply.github.com>
2022-01-29 12:26:33 -0600

    Initial commit

---
+++ create_users.ps1
@@ -0,0 +1,24 @@
+# ----- Edit these variables ----- #
+$PASSWORD_FOR_USERS   = "Password1"
+$USER_FIRST_LAST_LIST = Get-Content .\names.txt
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+
+foreach ($n in $USER_FIRST_LAST_LIST) {
+    $first = $n.Split(" ")[0]
+    $last = $n.Split(" ")[1]
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Green
+    
+    New-AdUser -GivenName $first `
+               -Surname $last `
+               -DisplayName $n `
+               -Name $username `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true `
+               -AccountPassword $password `
+               -PasswordNeverExpires $true `
+               -ChangePasswordAtLogon $false `
+}
